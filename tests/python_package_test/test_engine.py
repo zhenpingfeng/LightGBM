74b54d8c2b7731a31e06a96dbaa2816afcf099e4
Nikita Titov <nekit94-08@mail.ru>
2019-01-24 22:07:18 +0300

    Update test_engine.py

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -840,7 +840,7 @@ class TestEngine(unittest.TestCase):
         one_tree = model_str[model_str.find('Tree=1'):model_str.find('end of trees')]
         one_tree_size = len(one_tree)
         one_tree = one_tree.replace('Tree=1', 'Tree={}')
-        multiplier = int(2**14 / len(one_tree)) + 1
+        multiplier = int(2**31 / one_tree_size) + 1
         total_trees = multiplier + 2
         tree_sizes = model_str[model_str.find('tree_sizes'):model_str.find('Tree=0')].strip()
         tree_sizes = (tree_sizes
